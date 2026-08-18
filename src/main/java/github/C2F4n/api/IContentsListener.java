c2697e9c9d5a2aa8e402e9f377f0af325013dba6
root <baiyunziqulai@outlook.com>
2026-08-18 10:11:09 +0800

    forge_1.20.1_1.1

--- src/main/java/github/C2F4n/api/IContentsListener.java
+++
@@ -1,8 +0,0 @@
-package github.C2F4n.api;
-
-/** 内容变化监听：罐、槽、组件状态变化时回调，驱动配方缓存刷新与保存。 */
-@FunctionalInterface
-public interface IContentsListener {
-
-    void onContentsChanged();
-}
--- src/main/java/github/C2F4n/common/inventory/container/slot/ContainerSlotType.java
+++
@@ -1,7 +0,0 @@
-package github.C2F4n.common.inventory.container.slot;
-
-public enum ContainerSlotType {
-    INPUT,
-    OUTPUT,
-    NORMAL
-}
